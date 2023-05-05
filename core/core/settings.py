de301be04d16a6f9db3c645188b4a662b5393861
SepehrNorouzi7 <snorouzi07@gmail.com>
2023-05-05 10:34:37 +0430

    Add swagger and redoc setup
    First setup of swagger and redoc have been done

--- core/core/settings.py
+++ core/core/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'charity',
-    'rest_framework'
+    'rest_framework',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,16 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'HIVE Project API',
+    'DESCRIPTION': 'This is Hive project',
+    'VERSION': '1.0.0',
+}
 
 ROOT_URLCONF = 'core.urls'
 
--- core/core/urls.py
+++ core/core/urls.py
@@ -18,7 +18,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from charity.urls import *
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
 urlpatterns = [
+    path('api/schema', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('admin/', admin.site.urls),
     path('charity/', include('charity.urls'))
 ]
