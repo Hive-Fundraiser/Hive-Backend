06f57be074f25eed736d0f216a1f5d5a4a098a48
SepehrNorouzi7 <snorouzi07@gmail.com>
2023-05-16 20:26:25 +0430

    Update views of charity app
    comments removed.

--- core/charity/api/v1/urls.py
+++ core/charity/api/v1/urls.py
@@ -8,12 +8,6 @@ router.register('category',views.CategoryModelViewSet,basename='category')
 app_name = 'api-v1'
 
 urlpatterns = [
-    #path('ads/',views.ads_list,name='ads-list'),
-    #path('ads/',views.AdsList.as_view(),name='ads-list'),
-    #path('ads/<int:id>/',views.ads_detail,name='ads-detail'),
-    #path('ads/<int:pk>/',views.AdsDetail.as_view(),name='ads-detail'),
-    #path('ads/',views.AdsViewSet.as_view({'get':'list','post':'create'}),name='ads-list'),
-    #path('ads/<int:pk>/',views.AdsViewSet.as_view({'get':'retrieve','put':'update','patch':'partial_update','delete':'destroy'}),name='ads-detail'),
 
 ]
 
