bab126bcb03b795f08e3fd171a4a22c06846b054
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2023-05-26 13:03:00 +0330

    Update paginations.py

--- core/charity/api/v1/paginations.py
+++ core/charity/api/v1/paginations.py
@@ -1,7 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 class DefaultPagination(PageNumberPagination):
-    page_size = 12
+    page_size = 4
     def get_paginated_response(self, data):
         return Response({
             'links': {
