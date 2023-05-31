92b8ee638e3a690635127d46036f13b08b9ca83b
SepehrNorouzi7 <snorouzi07@gmail.com>
2023-05-31 15:03:38 +0430

    First setup of authentication

---
+++ core/accounts/api/v1/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path,include
+from .import views
+from rest_framework.routers import DefaultRouter
+
+urlpatterns = [
+
+]
+
--- core/core/urls.py
+++ core/core/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('charity/', include('charity.urls')),
     path('api-auth',include('rest_framework.urls'))
 ]
