babf2e297650fd5b5dd7856c0b967d6ad31edf1f
SepehrNorouzi7 <snorouzi07@gmail.com>
2023-06-01 17:41:02 +0430

    Customize token authentication response

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path,include
 from .import views
-from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import ObtainAuthToken
 
 app_name = 'api-v1'
 
 urlpatterns = [
-    path('registration/',views.RegistrationApiView.as_view(),name='registration')
+    path('registration/',views.RegistrationApiView.as_view(),name='registration'),
+    path('token/login/',views.CustomObtainAuthToken.as_view(),name='token-login'),
+    path('token/logout/',views.CustomDiscardToken.as_view(),name='token-logout'),
 ]
 
--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -1,7 +1,11 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import RegistrationSerializer
+from .serializers import RegistrationSerializer,CustomAuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 class RegistrationApiView(generics.GenericAPIView):
     serializer_class = RegistrationSerializer
@@ -15,3 +19,26 @@ class RegistrationApiView(generics.GenericAPIView):
             }
             return Response(data,status.HTTP_201_CREATED)
         return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
+
+class CustomObtainAuthToken(ObtainAuthToken):  
+    serializer_class = CustomAuthTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
+
+class CustomDiscardToken(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
