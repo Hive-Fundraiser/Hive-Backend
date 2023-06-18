35fd93c9876a87f3cdf11e440d3e33adb4e570e4
Mohammad Hussein Asnavandi <mhas.software@gmail.com>
2023-06-18 10:05:19 +0330

    add test for charity

--- core/charity/test/test_charity's_api.py
+++ core/charity/test/test_charity's_api.py
@@ -21,12 +21,12 @@ def common_user():
 
 @pytest.mark.django_db
 class TestPostApi:
-    def test_get_post_response_200_status(self, api_client):
+    def test_get_ad_response_200_status(self, api_client):
         url = reverse("charity:api-v1:ads-list")
         response = api_client.get(url)
         assert response.status_code == 200
 
-    def test_create_post_response_401_status(self, api_client):
+    def test_create_ad_response_401_status(self, api_client):
         url = reverse("charity:api-v1:ads-list")
         data = {
             "title": "test",
@@ -37,7 +37,7 @@ class TestPostApi:
         response = api_client.post(url, data)
         assert response.status_code == 401
 
-    def test_create_post_response_201_status(self, api_client, common_user):
+    def test_create_ad_response_201_status(self, api_client, common_user):
         url = reverse("charity:api-v1:ads-list")
         data = {
             "title": "test",
@@ -51,12 +51,37 @@ class TestPostApi:
         response = api_client.post(url, data)
         assert response.status_code == 201
 
-    def test_create_post_invalid_data_response_400_status(
+    def test_create_ad_invalid_data_response_400_status(
         self, api_client, common_user
     ):
         url = reverse("charity:api-v1:ads-list")
         data = {"title": "test", "content": "description" ,
         "status": True,"published_date": datetime.now(),}
+        user = common_user
+
+        api_client.force_authenticate(user=user)
+        response = api_client.post(url, data)
+        assert response.status_code == 400
+    
+@pytest.mark.django_db
+class TestDonationApi:
+    def test_create_donations_response_401_status(self, api_client):
+        url = reverse("charity:api-v1:donation-list")
+        data = {
+        "advertisement": 1,
+        "amount": 1
+        }
+        response = api_client.post(url, data)
+        assert response.status_code == 401
+
+    def test_create_donation_invalid_data_response_400_status(
+        self, api_client, common_user
+    ):
+        url = reverse("charity:api-v1:donation-list")
+        data = {
+        "advertisement": 1,
+        "amount": 1
+        }
         user = common_user
 
         api_client.force_authenticate(user=user)
