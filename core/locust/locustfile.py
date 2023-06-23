e7003927fff3cb9a06ddff9e78f96420ae6bd96b
PouyaAlavi2002 <pouya.alavi81@gmail.com>
2023-06-23 20:58:25 +0430

    Update locust.py and granted access and header

--- core/locust/locustfile.py
+++ core/locust/locustfile.py
@@ -2,14 +2,23 @@ from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
     
+    def on_start(self):
+        response = self.client.post('accounts/api/v1/jwt/create/' , data={
+            "emai":"admin@admin.com" ,
+            "password" : "a/@1234567"
+        }).json()
+        self.client.headers = {'Authorization' : f"Bearer {response.get('access' , None)}"}
+
+        
+
     @task
-    def advertisement_list(self):
+    def ads_list(self):
         self.client.get("/charity/api/v1/ads")
 
     @task
-    def advertisement_list(self):
+    def category_list(self):
         self.client.get("/charity/api/v1/category")
 
     @task
-    def advertisement_list(self):
+    def donation_list(self):
         self.client.get("/charity/api/v1/donation")
